80e2ebf8cbbef7430bb86095e443e73409600abf
Billy Hardy <billy92@gmail.com>
2019-03-15 04:21:18 +0000

    add webpack and ability to run in browser

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+module.exports = {
+      entry: './dist/plumbing/index.js',
+      output: {
+          filename: './bundle.js',
+          path: path.resolve(__dirname, 'dist/plumbing/')
+      },
+      mode: "development"
+};
+    //mode: "production"
